8b29397d6d23242674a2fe86e29e44d7d0c2435e
pipelonso <andresfelipeibanezcuta2@gmail.com>
2023-12-30 01:00:14 -0500

    Added some front on index, resources and languaje support

--- pages/resources/JS/index.js
+++ pages/resources/JS/index.js
@@ -1,15 +1,45 @@
 
 var contentpage = document.getElementById('contentPage'); 
+var estext = document.getElementById("esPresentacion");
+var entext = document.getElementById("enPresentation");
+
 
 document.addEventListener("DOMContentLoaded" , function(){
     
     console.log("loaded");
     var estext = document.getElementById("esPresentacion");
-    estext.style.display = "none";
+    //estext.style.display = "none";
     var lgSelector = document.getElementById('lgSelector');
-    lgSelector.value = "en";
+    
    
     contentpage.style.display = "none";
+
+    if (navigator.language.substring(0,2) == "en") {
+
+        entext.style.display = "block";
+        estext.style.display = "none";
+        lgSelector.value = "en";
+        
+        var infoStart = this.getElementById("infoStart");
+        infoStart.innerText = "Before you proceed please read this";
+
+
+    }else if (navigator.language.substring(0,2) == "es"){
+
+        entext.style.display = "none";
+        estext.style.display = "block";
+        lgSelector.value = "es";
+        var infoStart = this.getElementById("infoStart");
+        infoStart.innerText = "Porfavor lee esto antes de continuar";
+
+    }else{
+
+        entext.style.display = "block";
+        estext.style.display = "none";
+        lgSelector.value = "en";
+
+    }
+
 })
 
 
@@ -20,12 +50,27 @@ function hidestartermsg() {
     startermsg.style.display = "none";
     contentpage.style.display = "block";
 
+    var esparagrahp = document.getElementById("esparagrahp");
+    var enparagrahp = document.getElementById("enparagrahp");
+
+    if (navigator.language.substring(0,2) == "en") {
+
+        esparagrahp.style.display = "none";
+
+    }else{
+
+        enparagrahp.style.display = "none";
+
+    }
+
+
+
+
 }
 
 function startermsgLang(lang) {
 
-    var estext = document.getElementById("esPresentacion");
-    var entext = document.getElementById("enPresentation");
+    
 
     if (lang == "en") {
 
