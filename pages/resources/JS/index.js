fd8c82ae0f7551f41f343a344729ce6e11eedd13
pipelonso <andresfelipeibanezcuta2@gmail.com>
2024-01-21 02:23:16 -0500

    Remake index with vue.js and added some craftings

--- pages/resources/JS/index.js
+++ pages/resources/JS/index.js
@@ -1,90 +1,28 @@
 
-var contentpage = document.getElementById('contentPage'); 
-var estext = document.getElementById("esPresentacion");
-var entext = document.getElementById("enPresentation");
+var vm = new Vue({
+    el:'#app',
+    data() {
+        return {
+            lang: navigator.language.substring(0,2),
+            startmsglang : '',
+            startmsgshow : true
+        };
+    },
+    methods: {
+        hidestartermsg : function() {
+            this.startmsgshow = false;
+        },
+        startermsgLang : function() {
+            var lg = document.getElementById('lgSelector').value;
+            console.log(lg);
+        }
+    },
+    mounted() {
 
-
-document.addEventListener("DOMContentLoaded" , function(){
-    
-    console.log("loaded");
-    var estext = document.getElementById("esPresentacion");
-    //estext.style.display = "none";
-    var lgSelector = document.getElementById('lgSelector');
-    
-   
-    contentpage.style.display = "none";
-
-    if (navigator.language.substring(0,2) == "en") {
-
-        entext.style.display = "block";
-        estext.style.display = "none";
-        lgSelector.value = "en";
-        
-        var infoStart = this.getElementById("infoStart");
-        infoStart.innerText = "Before you proceed please read this";
-
-
-    }else if (navigator.language.substring(0,2) == "es"){
-
-        entext.style.display = "none";
-        estext.style.display = "block";
-        lgSelector.value = "es";
-        var infoStart = this.getElementById("infoStart");
-        infoStart.innerText = "Porfavor lee esto antes de continuar";
-
-    }else{
-
-        entext.style.display = "block";
-        estext.style.display = "none";
-        lgSelector.value = "en";
+        console.log('kboom');
 
     }
-
-})
-
+});
 
 
-function hidestartermsg() {
-
-    var startermsg = document.getElementById("startermsg");
-    startermsg.style.display = "none";
-    contentpage.style.display = "block";
-
-    var esparagrahp = document.getElementById("esparagrahp");
-    var enparagrahp = document.getElementById("enparagrahp");
-
-    if (navigator.language.substring(0,2) == "en") {
-
-        esparagrahp.style.display = "none";
-
-    }else{
-
-        enparagrahp.style.display = "none";
-
-    }
-
-
-
-
-}
-
-function startermsgLang(lang) {
-
-    
-
-    if (lang == "en") {
-
-        entext.style.display = "block";
-        estext.style.display = "none";
-
-
-    }else{
-
-        entext.style.display = "none";
-        estext.style.display = "block";
-
-    }
-
-
-}
 
